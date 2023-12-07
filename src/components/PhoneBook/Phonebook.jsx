b989d10ede62fe927fdd0d35c6d69d019c12181e
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-12-07 21:00:39 +0200

    result

--- src/components/PhoneBook/Phonebook.jsx
+++ src/components/PhoneBook/Phonebook.jsx
@@ -13,6 +13,19 @@ class Phonebook extends Component {
     number: '',
     showDeleted: false,
   }
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
 
   change = (e) => {
     const { name, value } = e.target;
